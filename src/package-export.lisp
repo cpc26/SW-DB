c28056718c358bea13e41671cda874193988adde
Lars Rune Nøstdal <lnostdal@blackbox.(none)>
2009-12-02 10:32:15 +0100

    Use AMX:EXPORT-ALL.

--- src/package-export.lisp
+++ src/package-export.lisp
@@ -4,8 +4,4 @@
 =common-headers=
 
 
-;; TODO: Describe why I do this in some blog-post or something. Short; this also exports symbols from CL, so users should use the SW-DB package instead of the CL package. The point -- besides me being lazy, is that conflicts can be dealt with _once_ and the result/solution trickles town to sub-sub-sub..-packages.
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (do-symbols (sym)
-    (export sym))
-  (export '(nil))) ;; CL pitfall..
+(export-all)
